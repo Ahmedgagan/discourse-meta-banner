1876c275f1a351149b28ab418ac220043bdb0e4b
Ahmed Gagan <ahmedgagan6@gmail.com>
2022-12-29 20:53:26 +0530

    DEV: completes feature

--- javascripts/discourse/components/meta-banner.js
+++ javascripts/discourse/components/meta-banner.js
@@ -5,13 +5,13 @@ import { inject as service } from "@ember/service";
 export default Component.extend({
   router: service(),
 
-  @discourseComputed("currentUser")
-  shouldShow(currentUser) {
-    const isStaff = currentUser && currentUser.staff;
-    const lowTrustLevel = currentUser && currentUser.trust_level < 2;
-    // show banner only for anons and < TL 2
-    return !isStaff && (!currentUser || lowTrustLevel);
-  },
+  // @discourseComputed("currentUser")
+  // shouldShow(currentUser) {
+  //   const isStaff = currentUser && currentUser.staff;
+  //   const lowTrustLevel = currentUser && currentUser.trust_level < 2;
+  //   // show banner only for anons and < TL 2
+  //   return !isStaff && (!currentUser || lowTrustLevel);
+  // },
 
   @discourseComputed("router.currentRouteName", "router.currentURL")
   discoveryRoute(currentRouteName, currentURL) {
